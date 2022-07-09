6b202500f05404c27d558b763f3dbebd0bba03a3
LuizHenrique000 <luizlhra24@gmail.com>
2022-07-08 21:51:35 -0300

    Conclui exercicio 1.8.3

---
+++ src/1.8.3/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  //https://www.auxiliadorapredial.com.br/alugar/residencial/rs+porto-alegre+cidade-baixa?quartos=2&sem-fiador-ou-garantias=true
+
+  //https
+  //protocolo
+
+  //www.auxiliadorapredial.com.br
+  //dominio
+
+  //alugar/residencial/rs+porto-alegre+cidade-baixa
+  //São as rotas
+
+  //quartos=2&sem-fiador-ou-garantias=true
+  //É a query
+
+  function criarUrl(protocolo, dominio, path, query) {
+    return `${protocolo}://${dominio}/${path}?=${query}`;
+  }
+
+  return (
+    <div>{criarUrl('https', 'www.google.com', 'search', 'Javascript')}</div>
+  );
+}
